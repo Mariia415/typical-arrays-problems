a7e9778bf647f7cb88f6f34570449ed41584e64a
Mariia415 <mariia.galitsyna@gmail.com>
2020-10-01 22:52:18 +0200

    update: functions min, max, avg
    no solution for undefined yet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 
 exports.min = function min (array) {
-  return 0;
-}
+    array.sort((a, b) => a -b);
+    return array[0];
+  }
 
 exports.max = function max (array) {
-  return 0;
+  array.sort((a, b) => a - b);
+    return array[array.length - 1];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  let count = array.length;
+  for(let i = 0; i < count; i++ ){
+    sum += array[i];
+  }
+  return sum / count;
 }
