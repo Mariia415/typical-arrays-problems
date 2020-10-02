7a01fcd4d3c03b185d002f397b4da91cea80b823
Mariia415 <mariia.galitsyna@gmail.com>
2020-10-02 12:23:26 +0200

    update: avg adjusted
    let sum and let count moved to proper block

--- src/index.js
+++ src/index.js
@@ -9,15 +9,23 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
+  if(typeof array === 'undefined' || array.toString() === ''){
+    return 0;
+  }else{
   array.sort((a, b) => a - b);
     return array[array.length - 1];
+ }
 }
 
 exports.avg = function avg (array) {
+  if(typeof array === 'undefined' || array.toString() === ''){
+    return 0;
+  }else{
   let sum = 0;
   let count = array.length;
   for(let i = 0; i < count; i++ ){
     sum += array[i];
   }
   return sum / count;
+ }
 }
